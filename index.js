b728fb57cbc8036e266e61a57799558039d96fb6
Hiago Oliveira <holiveira@tagplus.com.br>
2019-05-31 12:05:32 -0300

    adicionando dependencias

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ app = express();
 
 cron.schedule("* * * * *", () => {
     console.log("Executando a tarefa a cada 1 minuto");
-    //readExcel();
-    Post();
-    //get();
+    get();
 });
 
 app.listen(1313);
@@ -29,25 +27,35 @@ function Post (){
 }
 
 function get(){
+
+    var params = {
+        spreadsheetId: 'xxx',  // TODO: Update placeholder value.
+        range: 'Pedidos',  // TODO: Update placeholder value.
+    };
+    
     const axios = require('axios');
-    const url = "https://webhook.site/97b731f0-81bd-4791-bc72-0a2ca25b4a39";
+    const url = "https://sheets.googleapis.com/v4/spreadsheets/1xS2HaWzcSsRSWo1V96lcCde9CRhRZj904PHHX2Gb5OM/values/Pedidos?key=AIzaSyCvOKzgefAIc0k-HKPrZlrpE8KVeKAeWXo";
+    var retorno;
+    var str = "";
     try{
-        console.log(axios.get(url));
+        axios.get(url, { headers: { "Authorization": "Bearer ya29.GlwaB1o18UJosF4NpF0Bbb8ASjnPK8KtlbylxHq8lsnOQZa-N_OMVA9kjhgwA5vpzsO7i5dWLkTAU2iwuz8CtYFdkevERBNLVXr6wCYkjRjM1FGVvTcBK1RwMKhubg" } })
+        .then(response => {
+            retorno = response.data;
+            linhas = retorno.values;
+            //retorno = JSON.parse(retorno);
+            var count = linhas.length;
+            console.log(count + " pedidos encontrados");
+            for (var i in linhas){
+                val = linhas[i].toString();
+                str = val.split(",");
+                //console.log("linha " + i.toString() + " "   + str);
+            }
+        })
+        .catch((error) => {
+            console.log('error ' + error);
+        });
+
     }catch (error) {
         console.error(error);
     }    
-}
-
-function readExcel(){
-    var XLSX = require('xlsx')
-    var workbook = XLSX.readFile('ListaPedidosVenda-Grupo Restaurante.xlsx');
-    var sheet_name_list = workbook.SheetNames;
-    var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-    //console.log(xlData);
-    for (var i in xlData) {
-        val = xlData[i];
-        if(val.Número != ''){
-            console.log(val.Número);
-        }
-      }
 }
